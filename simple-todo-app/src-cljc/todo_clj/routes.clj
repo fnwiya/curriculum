abf42d3d5db8f09550a6c9cafe33218e1181eaa3
fnwiya <fnwiya@gmail.com>
2016-02-14 18:13:23 +0900

    update

--- simple-todo-app/src-cljc/todo_clj/routes.clj
+++ simple-todo-app/src-cljc/todo_clj/routes.clj
@@ -4,4 +4,8 @@
   ["/" {"" :home
         "todo" {"" :todo-index
                 ["/" [#"\d+" :id]] :todo-show}
+        "settings" {"/user" {"" :user-index
+                             "/add" {:get :user-add
+                                     :post :user-add-post}
+                             "/id" :user-show}}
         true :not-found}])
